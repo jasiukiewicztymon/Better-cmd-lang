cbce2dfefc0424f4628777ec36f22e5bb879b020
titi_2115 <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-01-31 22:49:42 +0100

    Update interpreter.h

--- interpreter.h
+++ interpreter.h
@@ -2,14 +2,16 @@
 #include <vector>
 #include <string>
 
-void interpret(std::string&, 
-std::map<std::string, int>&, 
-std::map<std::string, double>&,
-std::map<std::string, bool>&, 
-std::map<std::string, char>&, 
-std::map<std::string, std::string>&,
-std::map<std::string, std::vector<int>>&,
-std::map<std::string, std::vector<double>>&,
-std::map<std::string, std::vector<bool>>&,
-std::map<std::string, std::vector<char>>&,
-std::map<std::string, std::vector<std::string>>&);
+void interpret(
+	std::string&,
+	std::map<std::string, int>&,
+	std::map<std::string, double>&,
+	std::map<std::string, bool>&,
+	std::map<std::string, char>&,
+	std::map<std::string, std::string>&,
+	std::map<std::string, std::vector<int>>&,
+	std::map<std::string, std::vector<double>>&,
+	std::map<std::string, std::vector<bool>>&,
+	std::map<std::string, std::vector<char>>&,
+	std::map<std::string, std::vector<std::string>>&
+);
