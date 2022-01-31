5e934b4ec601b07f0a5954639e9466e6e59aa4f2
titi_2115 <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-01-31 23:34:20 +0100

    Update interpreter.h

--- interpreter.h
+++ interpreter.h
@@ -4,14 +4,6 @@
 
 void interpret(
 	std::string&,
-	std::map<std::string, int>&,
-	std::map<std::string, double>&,
-	std::map<std::string, bool>&,
-	std::map<std::string, char>&,
 	std::map<std::string, std::string>&,
-	std::map<std::string, std::vector<int>>&,
-	std::map<std::string, std::vector<double>>&,
-	std::map<std::string, std::vector<bool>>&,
-	std::map<std::string, std::vector<char>>&,
 	std::map<std::string, std::vector<std::string>>&
 );
