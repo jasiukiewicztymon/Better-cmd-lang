a86ea8cc2e6d375fd9d98385398498c6cd7db1c3
titi_2115 <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-01-31 23:34:44 +0100

    Update bettercmd.cpp

--- bettercmd.cpp
+++ bettercmd.cpp
@@ -9,21 +9,12 @@
 std::string command;
 int exited;
 
-std::map<std::string, int> IntVal;
-std::map<std::string, double> DoubleVal;
-std::map<std::string, bool> BoolVal;
-std::map<std::string, char> CharVal;
-std::map<std::string, std::string> StringVal;
-
-std::map<std::string, std::vector<int>> IntList;
-std::map<std::string, std::vector<double>> DoubleList;
-std::map<std::string, std::vector<bool>> BoolList;
-std::map<std::string, std::vector<char>> CharList;
-std::map<std::string, std::vector<std::string>> StringList;
+std::map<std::string, std::string> Val;
+std::map<std::string, std::vector<std::string>> List;
 
 int main(int argc, char* argv[]) {
     do {
         std::getline(std::cin, command);
-        interpret(command, IntVal, DoubleVal, BoolVal, CharVal, StringVal, IntList, DoubleList, BoolList, CharList, StringList);
+        interpret(command, Val, List);
     } while (command != "exit");
 }
