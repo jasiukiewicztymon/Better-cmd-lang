b7796a86a19037714f1d477eaec35e1dd7958c88
titi_2115 <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-02-08 22:46:45 +0100

    Update interpreter.h

--- interpreter.h
+++ interpreter.h
@@ -5,5 +5,7 @@
 void interpret(
 	std::string&,
 	std::map<std::string, std::string>&,
-	std::map<std::string, std::vector<std::string>>&
+	std::map<std::string, std::vector<std::string>>&,
+	std::map<std::string, std::string>&,
+	std::string&
 );
