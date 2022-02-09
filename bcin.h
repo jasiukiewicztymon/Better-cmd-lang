55ec01c36c2904a6cbce2e70386864cec729e8f1
titi_2115 <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-02-09 20:57:06 +0100

    Create bcin.h

---
+++ bcin.h
@@ -0,0 +1,6 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <Windows.h>
+
+void bcin(std::string&, std::string, std::map<std::string, std::string>, std::map<std::string, std::vector<std::string>>, std::vector<std::string>);
